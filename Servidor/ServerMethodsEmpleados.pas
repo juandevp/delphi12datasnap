2a3d500462c606f9fbe42e80a7ef12f98e3269ec
JCASALLAS <jcasallasnarvaez@gmail.com>
2024-07-17 19:45:28 -0500

    first commit

---
+++ Servidor/ServerMethodsEmpleados.pas
@@ -0,0 +1,21 @@
+unit ServerMethodsEmpleados;
+
+interface
+
+uses System.SysUtils, System.Classes, Datasnap.DSServer, Datasnap.DSAuth;
+
+type
+{$METHODINFO ON}
+  TServerMethods1 = class(TComponent)
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  published
+      { Metodos para declarar declarations }
+  end;
+{$METHODINFO OFF}
+
+implementation
+
+end.
